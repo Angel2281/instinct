03493c800001637f0af6aee9a2fad126c33dba3f
Angel2281 <32220820+Angel2281@users.noreply.github.com>
2018-06-03 22:12:38 +0300

    instinct
    dfdfdf

---
+++ js/config.js
@@ -0,0 +1,8 @@
+var l_musicPlaylist = [
+        {youtube: "IoLAV5_zm2Y", name: "Half life 2 Triage at Dawn (Extended)"}          
+];
+
+var l_musicVolume = 15;
+
+var youtubePlayer;
+var actualMusic = -1;
